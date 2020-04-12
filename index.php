f9992ac29a38b591eb2ef880622044ad2ae34e82
rydog1259 <60709963+rydog1259@users.noreply.github.com>
2020-04-11 20:59:28 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Home</title>
+</head>
+<body>
+  <form action="upload.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="file">
+    <button type="submit" name="submit">UPLOAD</button>
+  </form>
+</body>
---
+++ upload.php
@@ -0,0 +1,31 @@
+<?php
+if (isset($_post['submit'])) {
+  $files = $_FILES['file'];
+
+  $fileName = $_FILES['file']['name'];
+  $fileTmpName = $_FILES['file']['Tmp_name'];
+  $fileSize = $_FILES['file']['size'];
+  $fileError = $_FILES['file']['error'];
+  $fileType = $_FILES['file']['type'];
+
+  $fileExt = explode('.',$fileName);
+  $fileActualExt = strtolower(end($fileExt));
+
+  $allowed = $array('jpg', 'jpeg',);
+
+  if (in_array($fileActualExt, $allowed)) {
+    if ($fileError === 0) {
+      if ($filesSize < 5000000) {
+    }    else {
+            echo "Your files is too big!";
+        }
+    }   else {
+      Echo "there was and error with your file>";
+    }
+      }   else {
+    Echo "you can not upload thid kind of file.";
+  }
+}
+
+
+ ?>
